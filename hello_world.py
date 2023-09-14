018198310d4b45704360f31606153f1771266d9e
JunYezz <jyj1055@kumoh.ac.kr>
2023-09-14 11:07:56 +0900

    Update hello_world.py
    add print_KIT() func

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("computer engineering 20201023 JunYezz")
+
 if __name__ == '__main__':
-    print_hello()
+    print_KIT()
     
